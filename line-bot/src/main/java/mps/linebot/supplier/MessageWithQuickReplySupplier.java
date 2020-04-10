20c411b68e916a805652a1bed11b268daf51f1cc
sammy <sammy.chung@nexiosoft.com>
2020-04-10 17:57:18 +0800

    remove comment alt

--- line-bot/src/main/java/mps/linebot/supplier/MessageWithQuickReplySupplier.java
+++ line-bot/src/main/java/mps/linebot/supplier/MessageWithQuickReplySupplier.java
@@ -27,64 +27,54 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public class MessageWithQuickReplySupplier implements Supplier<Message> {
-    @Override
-    public Message get() {
+  @Override
+  public Message get() {
 
+    QuickReplyItem betCLI =
+        QuickReplyItem.builder()
+            .action(
+                new MessageAction(
+                    "投注指令",
+                    "您好, 投注指令格式为玩法＋金额, 例如庄100\n"
+                        + "\n"
+                        + "如果投注成功, 系统会回传讯息；如果投注不成功, 请确认投注格式是否有误, 或余额是否不足, 如有任何问题均可联系客服询问"))
+            .build();
+    QuickReplyItem awardCLI =
+        QuickReplyItem.builder()
+            .action(new MessageAction("打赏指令", "您好, 打赏指令格式为打赏＋金额, 例如打赏10"))
+            .build();
+    QuickReplyItem login =
+        QuickReplyItem.builder()
+            .action(new MessageAction("平台登錄", "开启 https://www.yabothai.com/"))
+            .build();
+    QuickReplyItem discount =
+        QuickReplyItem.builder().action(new MessageAction("最新优惠", "机器人丢出优惠活动")).build();
+    QuickReplyItem register =
+        QuickReplyItem.builder()
+            .action(new MessageAction("如何注册", "您好, 请点击「选单-会员注册」, 或联系客服询问"))
+            .build();
+    QuickReplyItem deposite =
+        QuickReplyItem.builder()
+            .action(new MessageAction("如何充值", "您好, 请点击「选单-会员充值」, 或联系客服询问"))
+            .build();
+    QuickReplyItem withdraw =
+        QuickReplyItem.builder()
+            .action(new MessageAction("如何提现", "您好, 提现请至娱乐城申请(https://yabothai.com),或联系客服询问"))
+            .build();
+    QuickReplyItem balance =
+        QuickReplyItem.builder()
+            .action(new MessageAction("如何查询余额", "您好, 请点击「选单-馀额查询」, 或联系客服询问"))
+            .build();
+    QuickReplyItem history =
+        QuickReplyItem.builder()
+            .action(new MessageAction("如何查询战绩", "您好, 请点击「选单-战绩查询」, 或联系客服询问"))
+            .build();
 
-        QuickReplyItem betCLI =
-                QuickReplyItem.builder()
-                        .action(
-                                new MessageAction(
-                                        "投注指令",
-                                        "您好, 投注指令格式为玩法＋金额, 例如庄100\n"
-                                                + "\n"
-                                                + "如果投注成功, 系统会回传讯息；如果投注不成功, 请确认投注格式是否有误, 或余额是否不足, 如有任何问题均可联系客服询问"))
-                        .build();
-        QuickReplyItem awardCLI =
-                QuickReplyItem.builder()
-                        .action(new MessageAction("打赏指令", "您好, 打赏指令格式为打赏＋金额, 例如打赏10"))
-                        .build();
-        QuickReplyItem login =
-                QuickReplyItem.builder()
-                        .action(new MessageAction("平台登錄", "开启 https://www.yabothai.com/"))
-                        .build();
-        QuickReplyItem discount =
-                QuickReplyItem.builder().action(new MessageAction("最新优惠", "机器人丢出优惠活动")).build();
-        QuickReplyItem register =
-                QuickReplyItem.builder()
-                        .action(new MessageAction("如何注册", "您好, 请点击「选单-会员注册」, 或联系客服询问"))
-                        .build();
-        QuickReplyItem deposite =
-                QuickReplyItem.builder()
-                        .action(new MessageAction("如何充值", "您好, 请点击「选单-会员充值」, 或联系客服询问"))
-                        .build();
-        QuickReplyItem withdraw =
-                QuickReplyItem.builder()
-                        .action(new MessageAction("如何提现", "您好, 提现请至娱乐城申请(https://yabothai.com),或联系客服询问"))
-                        .build();
-        QuickReplyItem balance =
-                QuickReplyItem.builder()
-                        .action(new MessageAction("如何查询余额", "您好, 请点击「选单-馀额查询」, 或联系客服询问"))
-                        .build();
-        QuickReplyItem history =
-                QuickReplyItem.builder()
-                        .action(new MessageAction("如何查询战绩", "您好, 请点击「选单-战绩查询」, 或联系客服询问"))
-                        .build();
+    final List<QuickReplyItem> items =
+        Arrays.<QuickReplyItem>asList(
+            betCLI, awardCLI, login, discount, register, deposite, withdraw, balance, history);
 
-        final List<QuickReplyItem> items =
-                Arrays.<QuickReplyItem>asList(
-                        betCLI, awardCLI, login, discount, register, deposite, withdraw, balance,
-                        history);
-
-        QuickReply quickReply = QuickReply.items(items);
-        TextMessage templateMessage = TextMessage.builder().text("快速查詢指令").quickReply(quickReply).build();
-
-
-
-            return TextMessage
-                .builder()
-                .text("Message with QuickReply")
-                .quickReply(quickReply)
-                .build();
-    }
+    QuickReply quickReply = QuickReply.items(items);
+    return TextMessage.builder().quickReply(quickReply).build();
+  }
 }
